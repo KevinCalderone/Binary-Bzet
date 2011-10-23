c8feb03267b5e98556fae74a3f877a2dd76d8145
Arash Sepasi Ahoei <arashahoei@gmail.com>
2011-10-22 17:09:20 -0700

    Added the typedef for our uint8_t and uint64_t				-Ash

--- Binary Bzet/Binary Bzet/BinaryBzet.h
+++ Binary Bzet/Binary Bzet/BinaryBzet.h
@@ -1,8 +1,12 @@
+#include <stdint.h>
+
+typedef uint8_t u8; // unsigned 8 bit int
+typedef uint64_t u64; // unsigned 64 bit int
+
 class BinaryBzet {
 public:
 	BinaryBzet();
 	~BinaryBzet();
-
 private:
-
+	u8 GetNumEndingZero(u64 i);
 };
