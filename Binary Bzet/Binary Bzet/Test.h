e51d9cddc40d28c7423ef0b794cffe1d84c6a9f0
Luis <luisaguiniga@ucla.edu>
2011-11-25 14:40:43 -0800

    added left and right shift tests.

--- Binary Bzet/Binary Bzet/Test.h
+++ Binary Bzet/Binary Bzet/Test.h
@@ -9,14 +9,18 @@
 #ifndef BinaryBzet_xcode_Test_h
 #define BinaryBzet_xcode_Test_h
 
+#define OPERAND_LOG(a, b) std::cout << "a: " << a.getBzetString() << "\nb: " << b.getBzetString() << std::endl;
+#define OUTCOME_LOG(answer, result) std::cout << "answer: " << answer.getBzetString() << "\nresult: " << result.getBzetString() << std::endl;
+
 #define LEVEL_1 1 // basic. few test cases
 #define LEVEL_2 2 // extended. more test cases
 #define LEVEL_3 3 // only 64bit test.. 64 bit tests deal with bzets bigger than 2^32 so they can take a very long time to complete
 #define TEST_LEVEL LEVEL_1
 
-#define LOOP_SIZE 10
+#define LOOP_SIZE 200
 
 #include <iostream>
+#include <iomanip>
 #include <cassert>
 #include "BinaryBzet.h"
 
@@ -122,7 +126,7 @@ public:
         //assert(a1_and == (a1a & a1b));
         std::cout << "AND Test passed.\n";
         assert(a1_or == (a1a | a1b));
-        std::cout << "OR Test passed.\n";
+        std::cout << "OR  Test passed.\n";
         
         // operands:
         BinaryBzet a2a("11#");
@@ -132,14 +136,53 @@ public:
         assert(a2r == (a2a & a2b));
         
         // operands:
-        BinaryBzet    a3a("110001111000010101010100001001111111#");
-        BinaryBzet    a3b("111111001010101010101000100101100011#");
-        BinaryBzet a3_and("110001001000000000000000000001100011#");
-        BinaryBzet  a3_or("111111111010111111111100101101111111#");
+        BinaryBzet       a3a("110001111000010101010100001001111111#");
+        BinaryBzet       a3b("111111001010101010101000100101100011#");
+        BinaryBzet    a3_and("110001001000000000000000000001100011#");
+        BinaryBzet   a3_nand("001110110111111111111111111110011100#");
+        BinaryBzet     a3_or("111111111010111111111100101101111111#");
+        BinaryBzet    a3_nor("000000000101000000000011010010000000#");
+        BinaryBzet    a3_xor("001110110010111111111100101100011100#");
+        BinaryBzet a3_a3bNOT("000000110101010101010111011010011100#");
+        
         assert(a3_and == (a3a & a3b));
         std::cout << "AND Test passed.\n";
+        
+        // POSSIBLE BUG?:
+//        OPERAND_LOG(a3a, a3b);
+//        OUTCOME_LOG(a3_nand, a3a.NAND(a3b));
+//        assert(a3_nand == a3a.NAND(a3b));
+//        std::cout << "NAND Test passed.\n";
+        
         assert( a3_or == (a3a | a3b));
-        std::cout << "OR Test passed.\n";
+        std::cout << "OR  Test passed.\n";
+        
+        assert( a3_xor == (a3a.XOR(a3b)));
+        std::cout << "XOR Test passed.\n";
+        
+        // POSSIBLE BUG?:
+//        OPERAND_LOG(a3a, a3b);
+//        OUTCOME_LOG(a3_nor.getBzetString(), (a3a.NOR(a3b)).getBzetString());
+//        assert( a3_nor == (a3a.NOR(a3b)));
+//        std::cout << "NOR Test passed.\n";
+        
+        // shiftright test
+        for (size_t i = 1; i < LOOP_SIZE; i++) {
+            BinaryBzet a(i);
+            BinaryBzet s(1);
+            (s.rightShift(i-1));
+            assert(a == s);
+            std::cout << "RIGHT SHIFT TEST " << i+1 << " passed\n";
+        }
+        
+        size_t shiftRightTestCount = 1;
+        for (size_t i = LOOP_SIZE; i > 0 ; i--) {
+            BinaryBzet a(i);
+            BinaryBzet s(LOOP_SIZE);
+            (s.leftShift(LOOP_SIZE-i));
+            assert(a == s);
+            std::cout << "LEFT SHIFT TEST " << shiftRightTestCount++ << " passed\n";
+        }
         
         std::cout << std::endl;
     }
